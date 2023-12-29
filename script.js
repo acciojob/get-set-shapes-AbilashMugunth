81f009bbe4d48f1980c3da641d6b4b7083644f90
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-29 19:48:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 class Rectangle {
   constructor(width, height) {
-    this._width = width;
-    this._height = height;
+    this.width = width;
+    this.height = height;
   }
 
   getWidth() {
-    return this._width;
+    return this.width;
   }
 
   getHeight() {
-    return this._height;
+    return this.height;
   }
 
   getArea() {
-    return this._width * this._height;
+    return this.width * this.height;
   }
 }
 
